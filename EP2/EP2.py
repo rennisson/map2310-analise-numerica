33de2d1a0ea5ba8cad68c545958048349ed1a4ce
rennisson <rennisson.alves@gmail.com>
2023-12-01 23:56:30 -0300

    coeficientes consertados

--- EP2/EP2.py
+++ EP2/EP2.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 
 def main():
     x = smp.symbols('x', real=True)
-    f = smp.exp(-x)
+    #f = smp.exp(-x)
+    f = smp.exp(x) * smp.sin(x) * smp.cos(x)
     text = input("Digite dois inteiros positivos para m e n: ")
     m, n = text.split(", ")
-    m = int(m)
-    n = int(n)
+    m, n = int(m), int(n)
     N = m + n + 1
     n += 1
-    q = np.zeros(N)  # Initializing q_i coefficients with zero
-    p = np.zeros(N)  # Initializing p_i coefficients with zero
+    p = np.zeros(n)  # Initializing p_i coefficients with zero
+    q = np.zeros(m)  # Initializing q_i coefficients with zero
 
     # STEP 1
     a = np.zeros(N)  # Initializing the maclaurin coefficients with zero
@@ -25,37 +25,6 @@ def main():
         dfdx = smp.diff(f, x, i)
         a[i] = dfdx.subs([(x, 0)]).evalf() / smp.factorial(i)
     print(f'{a=}')
-
-    # # # STEP 3
-    # # q[0] = 1
-    # # p[0] = a[0]
-    #
-    # # STEP 4
-    # for i in range(1, N+1):
-    #     # STEP 5
-    #     for j in range(1, i):
-    #         if j <= n:
-    #             b[i-1][j-1] = 0.
-    #
-    #     # STEP 6
-    #     if i <= n:
-    #         b[i-1][i-1] = 1
-    #
-    #     # STEP 7
-    #     for j in range(i+1, N+1):
-    #         b[i-1][j-1] = 0
-    #
-    #     # STEP 8
-    #     for j in range(1, i+1):
-    #         if j <= m:
-    #             b[i-1][n + j - 1] = -1 * a[i - j - 1]
-    #
-    #     # STEP 9
-    #     for j in range(n + i + 1, N+1):
-    #         b[i - 1][j - 1] = 0
-    #
-    #     # STEP 10
-    #     b[i-1][N] = a[i-1]
 
     for i in range(1, N + 1):
         for j in range(1, i):
@@ -80,91 +49,48 @@ def main():
     mat_1 = np.array_split(b, [N], axis=1)[0]
     mat_2 = np.array_split(b, [N], axis=1)[1]
     res = np.linalg.solve(mat_1, mat_2)
-    print(res)
-    print("\n")
 
-    p, q = sp.interpolate.pade(a, 2)
-    # q = np.flip(res[m + 1:].reshape(m))
-    # p = np.flip(res[:m + 1].reshape(n))
+    print(f'{res=}')
+    #### p, q = sp.interpolate.pade(a, 2)
+    p = np.flip(res[:n].reshape(1, n))
+    q = np.flip(res[n:].reshape(1, m))
+    p = np.append(np.array(p), [])
+    q = np.append(np.array(q), [1.])
     p = smp.Poly.from_list(p, x)
     q = smp.Poly.from_list(q, x)
+    r = p / q
 
+    # print(f'{p=}')
+    # print(f'{q=}')
 
-    # # STEP 11
-    # for i in range(n + 1, N):
-    #     # STEP 12
-    #     max_element = 0
-    #     k = 0
-    #     for j in range(i, N):
-    #         if np.abs(b[j][i]) > max_element:
-    #             max_element = np.abs(b[j][i])
-    #         for pivot in range(i, N+1):
-    #             if np.abs(b[pivot][i]) == np.abs(max_element):
-    #                 k = pivot
-    #                 break
-    #
-    #     # STEP 13
-    #     if b[k][i] == 0:
-    #         print("The system is singular")
-    #         break
-    #
-    #     # STEP 14
-    #     if k != i:  # (Interchange row i and row k.)
-    #         for j in range(i, N + 2):
-    #             b[i][j], b[k][j] = b[k][j], b[i][j]
-    #
-    #     # STEP 15
-    #     for j in range(i + 1, N):  # Perfom elimination
-    #         # STEP 16
-    #         try:
-    #             xm = b[j][i] / b[i][i]
-    #         except ZeroDivisionError as e:
-    #             print(e)
-    #             return
-    #
-    #         # STEP 17
-    #         for k in range(i + 1, N + 2):
-    #             b[j][k] = b[j][k] - xm * b[i][k]
-    #
-    #         # STEP 18
-    #         b[j][i] = 0
+    plt.figure()
+    plt.subplot(211)
+    x1 = np.linspace(0., 1.0, 300)
+    print(f'{x1.size} = tamanho de x1')
 
-    # # STEP 19
-    # if b[N-1][N] == 0:
-    #     print("The system is singular")
-    #     return
-    #
-    # # STEP 20
-    # if m > 0:  # Start backward substitution
-    #     try:
-    #         q[m] = b[N-1][N] / b[N-1][N]
-    #     except ZeroDivisionError as e:
-    #         print(e)
-    #         return
-    #
-    # # STEP 21
-    # for i in range(N - 1, n + 2, -1):
-    #     sum = 0
-    #     for j in range(i + 1, N+1):
-    #         sum += b[i][j] * q[j - n]
-    #
-    #     try:
-    #         q[i - n] = (b[i][N+1] - sum) / b[i][i]
-    #     except ZeroDivisionError as e:
-    #         print(e)
-    #         return
-    #
-    # # STEP 22
-    # for i in range(n, 0, -1):
-    #     sum = 0
-    #     for j in range(n+1, N+1):
-    #         sum += b[i][j] * q[j - n]
-    #
-    #     p[i] = b[i][N] - sum
+    # ERRO Q MEDIO, E CALCULO DE F(X)
+    f_values = []
+    r_values = []
+    max, ponto_max, erro_quadratico = 0, 0, 0
+    for value in x1:
+        y1, y2 = f.subs(x, value), r.subs(x, value)
+        f_values.append(y1)
+        r_values.append(y2)
+        erro_quadratico = np.sqrt(float((value - y1)**2) + float((value - y2)**2))
+        if erro_quadratico >= max:
+            ponto_max = value
+
+    f_values = np.array(f_values)
+    r_values = np.array(r_values)
 
     # STEP 23
+    print(f'{p=}')
     print(f'{q=}')
-    print(f'{p=}\n')
+    print(f'{r=}\n')
+
+    print(f'{erro_quadratico=}\n{ponto_max=}')
+    plt.plot(x1, f_values, 'k', x1, r_values, 'g--')
+    plt.show()
 
 
 main()
